74276fdd2f6b3690f36db524f699a6981a794189
cll <chenleilei_92@163.com>
2019-06-01 14:51:56 +0800

    提交sparklearn至github

---
+++ src/main/java/com/cll/test/Test.java
@@ -0,0 +1,9 @@
+package com.cll.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("this is my mac print");
+    }
+
+}
---
+++ src/main/scala/com/cll/sparklearn/Test.scala
@@ -0,0 +1,9 @@
+package com.cll.sparklearn
+
+object Test {
+
+  def main(args: Array[String]): Unit = {
+    print(1)
+  }
+
+}
